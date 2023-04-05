09befea7fc0f7e78f3b97f243cf984f7225c85db
Nyl <166322@student.horizoncollege.nl>
2023-04-05 14:59:04 +0200

    database setup

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use Twig\Loader\FilesystemLoader;
 use RedBeanPHP\R as R;
 
 require_once '../vendor/autoload.php';
+//connect to database
+R::setup('mysql:host=localhost;dbname=db', 'user', 'password');
 
 // template loader
 $loader = new \Twig\Loader\FilesystemLoader('../views');
