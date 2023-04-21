96b48835a81b44d221dd7ed2c6525516179020d2
Nyl <166322@student.horizoncollege.nl>
2023-04-21 08:56:15 +0200

    display post after created

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -2,6 +2,8 @@
 
 use RedBeanPHP\R as R;
 
+require_once "../public/api/code2img.php";
+
 class PostController extends BaseController
 {
     public function index()
@@ -24,5 +26,28 @@ class PostController extends BaseController
         );
         
         displayTemplate('post/create.twig', $const);
+    }
+    
+    // store new post in database
+    public function createPost()
+    {
+        // Create a new instance of the Code2ImgAPI class
+        $api = new Code2ImgAPI();
+
+        $code = $_POST['code'];
+        $theme = $_POST['theme'];
+        $language = $_POST['language'];
+
+        // Call the get_image method to get the image bytes
+        try {
+            $image_bytes = $api->get_image($code, $theme, $language);
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+            exit();
+        }
+
+        // Output the image to the browser
+        header('Content-Type: image/png');
+        echo $image_bytes;
     }
 }
---
+++ public/api/code2img.php
@@ -0,0 +1,80 @@
+<?php
+
+class Code2ImgAPI {
+    
+    private $base_url = "https://code2img.vercel.app/api/to-image";
+
+    private $valid_themes = array(
+        "a11y-dark",
+        "atom-dark",
+        "base16-ateliersulphurpool.light",
+        "cb",
+        "darcula",
+        "default",
+        "dracula",
+        "duotone-dark",
+        "duotone-earth",
+        "duotone-forest",
+        "duotone-light",
+        "duotone-sea",
+        "duotone-space",
+        "ghcolors",
+        "hopscotch",
+        "material-dark",
+        "material-light",
+        "material-oceanic",
+        "nord",
+        "pojoaque",
+        "shades-of-purple",
+        "synthwave84",
+        "vs",
+        "vsc-dark-plus",
+        "xonokai"
+    );
+
+    private $valid_languages = array(
+        "c",
+        "css",
+        "cpp",
+        "go",
+        "html",
+        "java",
+        "javascript",
+        "jsx",
+        "php",
+        "python",
+        "rust",
+        "typescript"
+    );
+
+    public function get_image($code, $theme = "default", $language = "plain") {
+        // Check if the theme and language are valid
+        if (!in_array($theme, $this->valid_themes)) {
+            throw new InvalidArgumentException("Invalid theme provided");
+        }
+        if (!in_array($language, $this->valid_languages)) {
+            throw new InvalidArgumentException("Invalid language provided");
+        }
+
+        $url = $this->base_url . "?theme=" . urlencode($theme) . "&language=" . urlencode($language);
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $code);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/plain"));
+        $response = curl_exec($ch);
+
+        // Check if the request was successful
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($http_code != 200) {
+            throw new Exception("Request failed with HTTP code " . $http_code);
+        }
+
+        curl_close($ch);
+
+        return $response;
+    }
+}
+
+?>
