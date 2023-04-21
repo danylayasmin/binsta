1cfbee6977734464f594215a398f5c4e7b29299f
Nyl <166322@student.horizoncollege.nl>
2023-04-21 11:43:09 +0200

    post linked to user

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -6,8 +6,40 @@ require_once "../public/api/code2img.php";
 
 class PostController extends BaseController
 {
-    public function index()
+    // show post with specified id
+    public function show()
     {
+        // Create a new instance of the Code2ImgAPI class
+        $api = new Code2ImgAPI();
+
+        // check if id is set
+        if (!isset($_GET['id'])) {
+            error(404, 'No ID provided', '/test/welcome');
+            exit;
+        }
+        
+        // check if id post exists
+        $post = $this->getBeanById('post', $_GET['id']);
+        if (!isset($post)) {
+            error(404, 'Post not found with ID ' . $_GET['id'], '/test/welcome');
+            exit;
+        }
+
+        $code = $post->code;
+        $theme = $post->theme;
+        $language = $post->language;
+
+        // Call the get_image method to get the image bytes
+        if (!isset($code) || !isset($theme) || !isset($language)) {
+            error(404, 'Post not found', '/test/welcome');
+            exit;
+        } else {
+            $image_bytes = $api->get_image($code, $theme, $language);
+        }
+
+        // Output the image to the browser
+        header('Content-Type: image/png');
+        echo $image_bytes;
     }
     
     // create new post
@@ -31,23 +63,18 @@ class PostController extends BaseController
     // store new post in database
     public function createPost()
     {
-        // Create a new instance of the Code2ImgAPI class
-        $api = new Code2ImgAPI();
+        // store data in database
+        $post = R::dispense('post');
+        $post->code = $_POST['code'];
+        $post->caption = $_POST['caption'];
+        $post->theme = $_POST['theme'];
+        $post->language = $_POST['language'];
+        $user = R::load('user', $_SESSION['loggedInUser']);
+        $post->user = $user;
+        R::store($post);
 
-        $code = $_POST['code'];
-        $theme = $_POST['theme'];
-        $language = $_POST['language'];
-
-        // Call the get_image method to get the image bytes
-        try {
-            $image_bytes = $api->get_image($code, $theme, $language);
-        } catch (Exception $e) {
-            echo "Error: " . $e->getMessage();
-            exit();
-        }
-
-        // Output the image to the browser
-        header('Content-Type: image/png');
-        echo $image_bytes;
+        // redirect to post
+        $id = $post->id;
+        header("Location: /post/show?id=$id");
     }
 }
